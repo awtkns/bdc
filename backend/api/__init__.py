fb6dbf6d030beac7a5006a083e30a8e4e34b96ee
Ethan Haid <ehaid@sfu.ca>
2020-05-29 03:16:19 -0700

    hey, charts sorta work now huh

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -2,14 +2,20 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS
 import pandas as pd
+import numpy as np
 
 from .data import analysis
 
 app = Flask(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-datasets = analysis.get_datasets()
+dataframes, datasets = analysis.get_datasets()
 slugs = [{'slug': str(key), 'title': datasets[key]['title']} for key in datasets.keys()]
+
+colours = []
+for _ in range(1000):
+    colour = str(list(np.random.choice(range(256), size=3))).replace("[", "rgb(").replace("]", ")")
+    colours.append(colour)
 
 
 @app.route('/')
@@ -17,26 +23,43 @@ def get_available_dataset():
     return {'slugs': slugs}, 200
 
 
-@app.route('/<slug>')
+@app.route('/dataset/<slug>')
 def get_dataset(slug):
+    return datasets[str(slug)], 200
+    
+
+@app.route('/barChartData/<slug>')
+def get_barChart(slug):
+
+    slug = 2  # TODO: !TEMPORARY! make it work with the other datasets lol
+
     col = request.headers.get('col')
-    data = datasets[str(slug)]
-    if col == None:
-        return data, 200
+    df = dataframes[str(slug)].loc[:, col]
 
-    # TODO: return the 1st and the 'col'th column of the dataset
-    df = pd.DataFrame([['Afghanistan', 0.2, 0.1], ['Albania', 5.36, 1.58], ['Algeria', 0, 0]], columns = ['Country', 'Computed Tomography', 'MRI'])
-    print(pd.DataFrame([df.iloc[:, 0], df.iloc[:, 2]], columns=['Country', 'MRI']))
+    # TODO: don't throw a fit if there's bad data in [countries] (ie, the user types something random)
+    countries = request.headers.get('countries')[1:-1].replace('"', "").split(',')
+    df = df.loc[countries]
 
-    labels = df.iloc[:, 0].to_string()
-    label = "MRI"
-    data = df.iloc[:, 2].to_string()
+    labels = df.index.values.tolist()
+    label = df.name
+    
+    data = df.values.tolist()
 
+    # TODO: pre-process this for efficiency
+    for i in range(len(data)):
+        if np.isnan(data[i]):
+            data[i] = 0 
 
+    chartData = {
+        'labels': labels,
+        'datasets': [
+            {
+                'label': label,
+                'data': data,
+                'backgroundColor': colours[:len(data)]
+            }
+        ]
+    }
 
-    "{labels: %s,datasets: [{label: %s,data: %s}]}" % (labels, label, data)
-    # backgroundColor: ["red", "orange", "yellow"]
-
-    print("{labels: %s,datasets: [{label: %s,data: %s}]}" % (labels, label, data))
-    return data, 200
+    return chartData, 200
 

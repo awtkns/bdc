95bc2fd8b2202563624daf13f4799219d4ee9310
adam-watkins <cadamrun@gmail.com>
2020-06-01 20:15:03 -0700

    styling report

---
+++ frontend/components/Abstract.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mt-4">
+    <v-row align="center" no-gutters>
+      <v-divider/>
+      <span class="mx-4 title" v-text="title" />
+      <v-divider/>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="11">
+        <p v-html="text" style="text-align: justify" />
+        <b>Keywords</b>
+        <v-row no-gutters>
+          <span  v-for="k in keywords" v-html="k" />
+        </v-row>
+      </v-col>
+    </v-row>
+
+    <v-divider />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Abstract",
+  props: { text: String, keywords: Array },
+  data: () => ({ title: 'Abstract' })
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,27 +1,29 @@
 <template>
   <v-row justify="center">
     <v-col lg="8" >
-      <div v-text="this.attributes.subtitle" class="subtitle text-center" />
-      <div v-text="this.attributes.title" class="title font-weight-bold mb-2 text-center" />
-      <div class="subtitle text-center">
-        <span v-for="a in this.attributes.authors" v-html="a" />
+      <div v-text="attributes.subtitle" class="subtitle text-center" />
+      <div v-text="attributes.title" class="title font-weight-bold mb-2 text-center" />
+      <div class="subtitle text-center mb-2">
+        <span v-for="a in attributes.authors" v-html="a" />
       </div>
-      <Manuscript class="text-left" />
-      <Datatable :datasetNames="datasetNames" />
+      <div v-for="a in attributes.affiliations" class="subtitle text-center" v-text="a" />
+      <Abstract :text="attributes.abstract" :keywords="attributes.keywords" class="mb-8"/>
+      <Manuscript class="text-justify" />
+<!--      <Datatable :datasetNames="datasetNames" />-->
     </v-col>
   </v-row>
 </template>
 
 <script>
 import Chart from "../components/Chart";
-import Datatable from "../components/Datatable";
-import fm from '~/articles/manuscript.md'
+import fm from '~/articles/main.md'
 import {getDatasetNames} from "../api";
+import Abstract from "../components/Abstract";
 
 export default {
   name: 'Blog',
   components: {
-    Datatable,
+    Abstract,
     Chart,
     Manuscript: {
       extends: fm.vue.component,
@@ -44,4 +46,7 @@ export default {
 * {
   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
 }
+  p {
+    text-align: justify;
+  }
 </style>
